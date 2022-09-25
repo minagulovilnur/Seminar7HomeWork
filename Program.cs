2a644badfdff53cda27a1b71be3bf119d4c92f2c
ilnur minagulov <ilnurminagulov@yandex.ru>
2022-09-25 21:02:37 +0300

    seminar7 homework

---
+++ Program.cs
@@ -0,0 +1,154 @@
+﻿/*
+Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+/*
+void RandomArrayNum(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(0, 1000))/10;// деление добавляет десятые, сотые и т.д.
+        }
+    }
+}
+void ShowArray(int[,]array){
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+            Console.WriteLine("");
+    }
+
+}
+
+Console.Write("Input numb of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input numb of cols: ");
+int cols = Convert.ToInt32(Console.ReadLine());
+
+int[,]array = new int[rows,cols];
+RandomArrayNum(array);
+ShowArray(array);
+
+
+/*
+Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение 
+этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+
+1 4 7 2
+5 9 2 3
+8 4 2 4
+
+1,7 -> такого числа в массиве нет
+1,2 -> 2
+
+
+
+void RandomArrayNum(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 1000);
+        }
+    }
+}
+void ShowArray(int[,]array){
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+            Console.WriteLine("");
+    }
+
+}
+
+
+Console.Write("Input numb of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input numb of cols: ");
+int cols = Convert.ToInt32(Console.ReadLine());
+
+int[,]array = new int[rows,cols];
+RandomArrayNum(array);
+ShowArray(array);
+Console.WriteLine();
+
+Console.Write("Введите номер строки искомого элемента: ");
+int rows1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите номер столбца искомого элемента: ");
+int cols1 = Convert.ToInt32(Console.ReadLine());
+
+if (rows1 < array.GetLength(0) && cols1 < array.GetLength(1)) Console.WriteLine(array[rows1, cols1]);
+else Console.WriteLine($"Вы указали элемент с позицией [{rows1},{cols1}] -> такого числа с указазнной позицией в массиве нет");
+*/
+
+
+
+/*
+Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+
+void RandomArrayNum(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+void ShowArray(int[,]array){
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+            Console.WriteLine("");
+    }
+
+}
+
+
+Console.Write("Input numb of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input numb of cols: ");
+int cols = Convert.ToInt32(Console.ReadLine());
+
+int[,]array = new int[rows,cols];
+RandomArrayNum(array);
+ShowArray(array);
+Console.WriteLine();
+
+
+
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum += array[i, j];
+    }
+    Console.Write($" { sum / array.GetLength(0)} ");
+}
+    
